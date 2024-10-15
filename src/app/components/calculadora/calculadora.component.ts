17c371daa526a55fb079de82af9661b595625ba8
Manuel <gomez.maman24@triana.salesianos.edu>
2024-10-15 14:37:09 +0200

    Hecho y mejorado

--- src/app/components/calculadora/calculadora.component.ts
+++ src/app/components/calculadora/calculadora.component.ts
@@ -5,56 +5,89 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './calculadora.component.html',
   styleUrl: './calculadora.component.css'
 })
+
+// export class CalculadoraComponent {
+//   numero1: string = '';
+//   acumulador: number = 0;
+//   signo: string = '';
+//   resultado: number = 0;
+
+//   anadirDigito(digito: string) {
+//     this.numero1 += digito;
+//   }
+
+//   anadirOperacion(operacion2: string) {
+
+//     if (this.numero1 !==''){
+
+//     }
+
+//   }
+
+//   anadirResultado() {
+//     this.acumulador = Number(this.numero1);
+
+//     if (this.signo === '+') {
+//       this.resultado = this.acumulador + this.acumulador;
+//     } else if (this.signo === '-') {
+//       this.resultado = this.acumulador - this.acumulador;
+//     }
+
+//     this.numero1 = this.resultado.toString();
+//   }
+
+
+//   //Limpiar pantalla
+//   limpiarPantalla(){
+//     this.numero1 = '';
+//     this.acumulador = 0;
+//     this.signo = '';
+//     this.resultado = 0;
+//   }
+// }
 export class CalculadoraComponent {
-
-
-
-
-
- 
-  numero1  ='';
-  num = 0;
-  num2 = 0;
-
-  resultado = 0;
-
-  // anadirDigito() {
-  //   if (this.operando == "+" || this.operando == "-") {
-
-  //   }
-
-  // }
+  numero1: string = '';
+  acumulador: number = 0;
+  signo: string = '';
+  resultado: number = 0;
 
   anadirDigito(digito: string) {
-   
     this.numero1 += digito;
-   
-  }
-
-
-
-  anadirResultado(operacion: string) {
-    if(operacion === '+') {
-      this.resultado = this.num + this.num2;
-      this.numero1=this.resultado.toString();
-      return this.numero1;
-    } else if(operacion === '-') {
-      this.resultado = this.num - this.num2;
-      this.numero1 = this.resultado.toString();
-      return this.numero1;
-    } else {
-      return this.numero1;
-    }
   }
 
   anadirOperacion(operacion: string) {
-    if (operacion === '+' || operacion === '-') {
+    if (this.numero1 !== '') {
+      if (this.signo !== '') {
+        this.realizarOperacion();
+      } else {
+        this.acumulador = Number(this.numero1);
+      }
+      this.signo = operacion;
       this.numero1 = '';
-      this.anadirDigito;
-      this.num2 = Number(this.numero1);
-    } else {
-      this.anadirDigito;
-      this.num = Number(this.numero1);
     }
+  }
+
+  realizarOperacion() {
+    const num = Number(this.numero1);
+    if (this.signo === '+') {
+      this.acumulador += num;
+    } else if (this.signo === '-') {
+      this.acumulador -= num;
+    }
+  }
+
+  anadirResultado() {
+    if (this.numero1 !== '') {
+      this.realizarOperacion();
+      this.numero1 = this.acumulador.toString();
+      this.signo = '';
+    }
+  }
+
+  limpiarPantalla() {
+    this.numero1 = '';
+    this.acumulador = 0;
+    this.signo = '';
+    this.resultado = 0;
   }
 }
